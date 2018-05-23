56a11e2459778c1b8ead50b94e1d599e2586af34
Quin <qcarnahanhome@gmail.com>
2018-05-22 21:27:50 -0700

    help

--- reply_post.py
+++ reply_post.py
@@ -2,24 +2,78 @@ import praw
 import pdb
 import re
 import os
+import time
 
-reddit = praw.Reddit('bot1')
-if not os.path.isfile("posts_replied_to.txt"):
-	print("creating posts_replied_to")
-	posts_replied_to = []
-else:
-	with open("posts_replied_to.txt", "r") as f:
-		posts_replied_to = f.read()
-		posts_replied_to = posts_replied_to.split("\n")
-		posts_replied_to = list(filter(None, posts_replied_to))
+ # FILTERED WORDS BELOW
+
+
+
+
+
+
+
+
+FILTEREDWORDS = ['nigger','fuck','damn','bitch','whore','slut','cunt','ass','asshole','damnit','dick', 'heck']
+MODREASON = ["You have violated rule #1 no swearing!"]
+
+
+
+
+
+
+
+def run_bot(r):
+	if not os.path.isfile("posts_replied_to.txt") or not os.path.isfile("comments_replied_to.txt"):
+		print("creating posts_replied_to")
+		print("creating comments_replied_to")
+		posts_replied_to = []
+		comments_replied_to = []
+	else:
+		with open("posts_replied_to.txt", "r") as f:
+			posts_replied_to = f.read()
+			posts_replied_to = posts_replied_to.split("\n")
+			posts_replied_to = list(filter(None, posts_replied_to))
+		with open("comments_replied_to.txt", "r") as c:
+			comments_replied_to = c.read()
+			comments_replied_to = comments_replied_to.split("\n")
+			comments_replied_to = list(filter(None, comments_replied_to))
+
+
 	
-subreddit = reddit.subreddit('dank_APEuro')
-for submission in subreddit.hot(limit=5):
-	if submission.id not in posts_replied_to:
-		if re.search("communist", submission.title, re.IGNORECASE):
-			submission.reply("TEST BOT says: COMMIE!")
-			print("Bot replying to : ", submission.title)
+	for submission in subreddit.hot(limit=25):
+		process_submission(submission)
+
+			# if re.search("communist", submission.title, re.IGNORECASE):
+			# 	submission.reply("TEST BOT says: COMMIE!")
+			# 	print("Bot replying to : ", submission.title)
+			# 	posts_replied_to.append(submission.id)
+			# 	with open("posts_replied_to.txt", "w") as f:
+		 #    				for post_id in posts_replied_to:
+		 #    					f.write(post_id + "\n")
+
+
+
+
+def process_submission(submission):
+#	if submission.id not in posts_replied_to:
+	normalized_title = submission.title.lower()
+	for word in FILTEREDWORDS:
+		if word in normalized_title:
+			submission.mod.remove()
 			posts_replied_to.append(submission.id)
-			with open("posts_replied_to.txt", "w") as f:
-	    				for post_id in posts_replied_to:
-	    					f.write(post_id + "\n")
+	submission.comments.replace_more(limit=None)
+	for comment in submission.comments.list():
+#				if comment.id not in comments_replied_to:
+		
+		for word in FILTEREDWORDS:
+			if word in comment.body.lower():
+				comment.mod.remove()
+				author = comment.author
+				print("Banning ", author)
+				subreddit.banned.add(author, params={"duration": 7, "note": MODREASON[0], "ban_message": "TEST"})
+				# comment_replied_to.append(comment.id)
+
+
+r = praw.Reddit('bot1')
+subreddit = r.subreddit('dank_APEuro')
+run_bot(r)
